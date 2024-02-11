06018531cf8b8b7455443dbec16479f394c67cd4
Dawid Debosz <davelikme@gmail.com>
2024-02-11 22:33:36 +0100

    markup, connect gpt, stream chat output

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Chat from "@/components/Chat";
+import { cn } from "@/lib/utils";
+import Providers from "@/components/Providers";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Bookbuddy",
+  description: "Your bookstore for fantasy and mystery novels",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <Providers>
+        <body className={inter.className}>
+          <Chat />
+          {children}
+        </body>
+      </Providers>
+    </html>
+  );
+}
